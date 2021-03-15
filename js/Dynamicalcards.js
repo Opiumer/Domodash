9a899a21127ef99b435aae196099a7d2dcbbe261
Opiumer <omardrochon@gmail.com>
2021-03-15 20:18:27 +0100

    test dynamical cards

---
+++ js/Dynamicalcards.js
@@ -0,0 +1,6 @@
+<script> 
+        function addCode() { 
+            document.getElementById("cardSlot").innerHTML +=  
+              "<h3>This is the text which has been inserted by JS</h3>"; 
+        } 
+</script>
